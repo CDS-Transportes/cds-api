343f6615445a575634f201c9540b9a5b6afa6d52
LukaOliveira <lukaoliveira1401@gmail.com>
2022-04-15 17:40:32 -0300

    Exemplo Registro

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, request
+from flask_cors import cross_origin
 from user_methods.pf_manage import PFManage
 
 cds_api = Flask(__name__)
 
-@cds_api.route("/pfuser", methods=['POST', 'GET'])
+@cds_api.route("/pfuser", ethods=['POST', 'GET'])
+@cross_origin()
 def pfuser_route():
     respose = PFManage(request).direct_method()
     return respose
-    
+
 @cds_api.route("/pjuser", methods=['POST', 'GET'])
+@cross_origin()
 def pjuser_route():
     return 'None'
 
