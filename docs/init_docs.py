db1c3cbcaec702c180025dcf408426919368832b
LukaOliveira <lukaoliveira1401@gmail.com>
2022-04-17 18:41:00 -0300

    Remocao exemplos, adicao swagger

---
+++ docs/init_docs.py
@@ -0,0 +1,16 @@
+from flask_restx import Api, fields
+
+def init_docs(app):
+    api = Api(app,
+            version='1.0',
+            title='API CDS Transportes',
+            description='Documentação API CDS Transportes',
+            doc='/'
+            )
+
+
+
+    
+
+
+    
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from flask_cors import cross_origin
+from docs.init_docs import init_docs
 
 from methods.register_method import register_method
 from methods.login_method import login_method
 
 cds_api = Flask(__name__)
-
+init_docs(cds_api)
 
 @cds_api.route("/api/v1/register", methods=['POST'])
 @cross_origin()
